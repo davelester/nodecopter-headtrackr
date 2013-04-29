fcbc04b1c79b05213465ef83f5a2162c9c9fbd97
Dave Lester <davelester@gmail.com>
2013-04-29 16:03:22 -0700

    First push of headtrackr drone app.

--- app.js
+++ app.js
@@ -19,8 +19,13 @@ app.use(express.static(path.join(__dirname, 'static')));
 // Routes
 const index = require('./controllers/index');
 app.get('/', index.home);
+app.get('/turnleft', index.turnleft)
+app.get('/hover', index.hover)
 
 var server = http.createServer(app);
+
+drone.takeoff();
+
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,16 @@
+var arDrone = require('ar-drone');
+var drone  = arDrone.createClient();
+
 exports.home = function(request, response) {
 	return response.render('home.html');
+}
+
+exports.turnleft = function(request, response) {
+	// execute the nodejs command to turn this
+	drone.clockwise(1);
+	console.log('turning left!')
+}
+
+exports.hover = function(request, response) {
+	drone.stop()
 }
